f33ce073128ffb0bc5271ad0bb61149f40e1d1c3
skarpecki <skarpecki@users.noreply.github.com>
2022-01-06 21:24:25 +0100

    Added exercises functionality

---
+++ frontend/html/exercises/script.js
@@ -0,0 +1,74 @@
+
+window.onload = function() {
+    
+    const exercisesURL = "http://127.0.0.1:5000/exercises/"
+    
+    fetch(exercisesURL, { method: "GET", redirect: "follow" }).then(function(response){
+        return response.json();
+    }).then(function(jsonArray) {
+        fillExerciseSelector(jsonArray);
+    }).catch(function(err) {
+        console.log(err.message);
+    });
+    
+    document.getElementById("exercise-name-selector").ondblclick = function() {
+        const exerciseArticle = document.querySelector("article");
+        
+        // getExerciseJSON(this.value).then(function(json){
+        //     exerciseArticle.innerHTML = getExerciseArticleHTML(json);
+        // }); 
+        
+        getExerciseJSON(this.value).then(json => 
+            exerciseArticle.innerHTML = getExerciseArticleHTML(json));
+    }
+    
+    
+}
+
+function getExerciseArticleHTML(json) {
+    let name = `<h1>${json.name}</h1>`;
+    let type = `<h2>${json.type.charAt(0).toUpperCase() + json.type.slice(1).toLowerCase()} exercise</h2>`;
+    let description = ``;
+    if(("description" in json) == true)
+    {
+        description = `<h3>Description</h3>
+                    <p>${json.description}</p>`;
+    }
+    let additionalDescription = ``;
+    /* if(athlete in addtional description)
+     * {
+     *   create additional description part  
+     * }
+     */
+    let video_url = ``;
+    if(("video_url" in json) == true)
+    {
+        video_url = `<iframe width="560" height="315" src="${json.video_url}" title="YouTube video player" frameborder="0" allowfullscreen></iframe>`;
+    }
+
+    return `${name}
+     ${type}
+     ${description}
+     ${additionalDescription}
+     ${video_url}`
+}
+
+function getExerciseJSON(exerciseID) {
+    let exerciseUrl = `http://127.0.0.1:5000/exercises/${exerciseID}`;
+    return fetch(exerciseUrl).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        return json;
+    }).catch(function(err) {
+        console.log(err.message);
+    })
+}
+
+var fillExerciseSelector = jsonArray => {
+    const select = document.querySelector("select")
+    jsonArray["exercises"].forEach(element => {
+        let opt = document.createElement("option");
+        opt.value=element._id;
+        opt.text=element.name;
+        select.appendChild(opt);
+})};
--- rest_api/resource_exercises/controller.py
+++ rest_api/resource_exercises/controller.py
@@ -8,37 +8,31 @@ exercises_bp = Blueprint('exercises', __name__)
 @exercises_bp.route("/", methods=['GET', 'POST'])
 def exercises():
     if request.method == 'GET':
-        return exercisesGet()
+        filter={}
+        result = ExerciseService(r"mongodb://localhost:27017",
+                                "dashboard",
+                                "exercises").get_exercises(filter)
     else:
-        return exercisesPost(request.get_json(force=True))
+        exercise_json = request.get_json(force=True)
+        result = ExerciseService(r"mongodb://localhost:27017",
+                                  "dashboard",
+                                  "exercises").insert_exercise(exercise_json)
+    resp = make_response(result.json_message, result.http_code)
+    resp.headers["Content-Type"] = "application/json"
+    resp.headers["Access-Control-Allow-Origin"] = "http://localhost:9000"
+    resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return resp    
+
+
 
 @exercises_bp.route("/<exercise_id>", methods=['GET', 'PUT'])
 def exercise(exercise_id):
     if request.method == 'GET':
-        return exerciseGetByID(exercise_id)
-
-def exercisesGet(filter={}):
-    result = ExerciseService(r"mongodb://localhost:27017",
-                                "dashboard",
-                                "exercises").get_exercises(filter)
-    resp = make_response(result.json_message, result.http_code)
-    resp.headers["mimetype"] = "application/json"
-    return resp
-
-def exerciseGetByID(exercise_id):
-    result = ExerciseService(r"mongodb://localhost:27017",
+        result = ExerciseService(r"mongodb://localhost:27017",
                                 "dashboard",
                                 "exercises").get_exercise(exercise_id)
-    resp = make_response(result.json_message, result.http_code)
-    resp.headers["mimetype"] = "application/json"
-    return resp 
-
-def exercisesPost(exercise_json):
-    result = ExerciseService(r"mongodb://localhost:27017",
-                                  "dashboard",
-                                  "exercises").insert_exercise(exercise_json)
-    resp = make_response(result.json_message, result.http_code)
-    resp.headers["mimetype"] = "application/json"
-    return resp
-
-
+        resp = make_response(result.json_message, result.http_code)
+        resp.headers["Content-Type"] = "application/json"
+        resp.headers["Access-Control-Allow-Origin"] = "http://localhost:9000"
+        resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return resp 

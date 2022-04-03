409698d76c319bef044f0088e6b0a2f6280b9f60
skarpecki <skarpecki@users.noreply.github.com>
2022-04-03 17:17:14 +0200

    Added user schema

--- db/create.sql
+++ db/create.sql
@@ -1,4 +1,7 @@
-CREATE TABLE users(
+CREATE DATABASE IF NOT EXISTS `athletes_db`;
+USE `athletes_db`;
+
+CREATE TABLE IF NOT EXISTS users(
 id INT primary key auto_increment,
 mail_address varchar(100) not null,
 password varchar(100) not null
--- rest_api/resource_users/model.py
+++ rest_api/resource_users/model.py
@@ -1,5 +1,14 @@
 from app import db
 from enum import Enum
+from marshmallow import fields, Schema
+
+
+class UserSchema(Schema):
+    """User Schema"""
+
+    userID = fields.Integer(attribute='id')
+    mailAddress = fields.String(attribute='mail_address')
+    password = fields.String(attribute='password')
 
 class User(db.Model):
     """User model class"""
@@ -9,4 +18,3 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mail_address = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-
